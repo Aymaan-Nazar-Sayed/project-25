40ec9cec1d4fbfde227c0087963421a6cea0b010
Aymaan-Nazar-Sayed <72451748+Aymaan-Nazar-Sayed@users.noreply.github.com>
2020-12-11 09:46:10 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,9 @@
+class Ball extends BaseClass{
+    constructor(x,y,width,height){
+        super(x.y,20,width,height)
+        this.image = loadImage("sprites/paper.png");
+        }
+        
+
+
+    }
